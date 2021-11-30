2c0e859651e12a38bfde8a21407be948cafe0ab6
Joshua Dane <joshrdane@gmail.com>
2021-11-30 07:53:39 -0500

    Removed #isPalindrome(char[]) and #longestPalindrome(char[])

--- src/LongestPalindromicSubstring.java
+++ src/LongestPalindromicSubstring.java
@@ -1,5 +1,5 @@
 public class LongestPalindromicSubstring {
-    public static String oldLongestPalindrome(String string) {
+    public static String longestPalindrome(String string) {
         int index = 0;
         int length = 0;
         for (int i = 0; i < string.length(); i++) {
@@ -21,45 +21,5 @@ public class LongestPalindromicSubstring {
             }
         }
         return true;
-    }
-
-    public static String longestPalindrome(String string) {
-        return longestPalindrome(string.toCharArray());
-    }
-
-    public static String longestPalindrome(char[] array) {
-        int index = 0;
-        int length = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + length; j < array.length; j++) {
-                if (isPalindrome(array, i, j)) {
-                    length = j - i + 1;
-                    index = i;
-                }
-            }
-        }
-        StringBuilder builder = new StringBuilder();
-        for (int i = index; i < index + length; i++) {
-            builder.append(array[i]);
-        }
-        return builder.toString();
-    }
-
-    public static boolean isPalindrome(char[] array) {
-        return isPalindrome(array, 0, array.length - 1);
-    }
-
-    public static boolean isPalindrome(char[] array, int start, int end) {
-        for (int i = start; i < start + (end - start) / 2; i++) {
-            System.out.println(i);
-            if (array[i] != array[end - i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(longestPalindrome("abad"));
     }
 }
