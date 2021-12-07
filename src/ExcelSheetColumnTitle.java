23dbea93ee604625bdcb51da5f5515065816391a
Joshua Dane <joshrdane@gmail.com>
2021-12-07 12:44:16 -0500

    Faster than 100% of submissions, less memory than 97.61%

--- src/ExcelSheetColumnTitle.java
+++ src/ExcelSheetColumnTitle.java
@@ -1,2 +1,17 @@
 public class ExcelSheetColumnTitle {
+    public static String convertToTitle(int columnNumber) {
+        StringBuilder result = new StringBuilder();
+        while (columnNumber-- > 0) {
+            result.insert(0, ((char) (columnNumber % 26 + 65)));
+            columnNumber /= 26;
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convertToTitle(1));
+        System.out.println(convertToTitle(28));
+        System.out.println(convertToTitle(701));
+        System.out.println(convertToTitle(2147483647));
+    }
 }
