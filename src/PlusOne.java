0180ba3f9cdb7af9dd37f92bbcf8952852072153
Joshua Dane <joshrdane@gmail.com>
2022-05-30 15:38:13 -0400

    Faster than 100%

---
+++ src/PlusOne.java
@@ -0,0 +1,14 @@
+public class PlusOne {
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[digits.length + 1];
+        System.arraycopy(digits, 0, result, 1, result.length - 1);
+        return result;
+    }
+}
