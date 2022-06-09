8218690384e2e03b3d4e69bb71f7d92b19fd8a86
Joshua Dane <joshrdane@gmail.com>
2022-06-09 12:17:20 -0400

    Accepted

---
+++ src/ConcatenationOfArray.java
@@ -0,0 +1,9 @@
+public class ConcatenationOfArray {
+    public static int[] getConcatenation(int[] nums) {
+        int[] result = new int[nums.length * 2];
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = result[i + nums.length] = nums[i];
+        }
+        return result;
+    }
+}
