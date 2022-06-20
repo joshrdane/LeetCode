c76d6a36efb9356d0faeff5c048d8c9d1306a691
Joshua Dane <joshrdane@gmail.com>
2022-06-20 07:34:28 -0400

    LeetCode solution

---
+++ src/ShortEncodingOfWords.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ShortEncodingOfWords {
+    public static int minimumLengthEncoding(String[] words) {
+        Set<String> good = new HashSet<>(Arrays.asList(words));
+        for (String word : words) {
+            for (int k = 1; k < word.length(); ++k)
+                good.remove(word.substring(k));
+        }
+
+        int ans = 0;
+        for (String word: good)
+            ans += word.length() + 1;
+        return ans;
+    }
+}
