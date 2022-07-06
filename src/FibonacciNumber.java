5df34a9106f12b33d9786f1d4ca166267936ffb4
Joshua Dane <joshrdane@gmail.com>
2022-07-06 07:37:27 -0400

    Accepted one-liner

--- src/FibonacciNumber.java
+++ src/FibonacciNumber.java
@@ -1,9 +1,5 @@
 public class FibonacciNumber {
     public static int fib(int n) {
-        return switch (n) {
-            case 0 -> 0;
-            case 1 -> 1;
-            default -> fib(n - 1) + fib(n - 2);
-        };
+        return n < 2 ? n : fib(n - 1) + fib(n - 2);
     }
 }
